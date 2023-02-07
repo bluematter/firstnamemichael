2e69a691241d3c3fdad2401514313ae4b9022206
bluematter <michael@motionbox.io>
2023-02-06 20:50:12 -0800

    chore: add postmark

--- packages/prisma/api/mutations/user.ts
+++ packages/prisma/api/mutations/user.ts
@@ -1,4 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import postmark from "postmark";
+
 import { IBuilder } from "../schema";
 import { PrismaClient } from "../../prisma/client";
 
@@ -11,6 +13,29 @@ const userMutation = (db: PrismaClient, builder: IBuilder) => {
       },
       resolve: async (query, root, { email }) => {
         try {
+          const client = new postmark.ServerClient(
+            "3aa81f8d-6f8b-4279-aa9c-51bd473a1fee"
+          );
+
+          client.sendEmailWithTemplate({
+            From: "michael@michaelaubry.com",
+            To: email,
+            TemplateAlias: "newsletter-confirmation",
+            TemplateModel: {
+              product_url: "michaelaubry.com",
+              product_name: "Michael Aubry",
+              action_url: "action_url_Value",
+              company_name: "Michael Aubry",
+              company_address: "company_address_Value",
+              name: "Michael Aubry",
+              invite_sender_name: "Michael Aubry",
+              invite_sender_organization_name: "Michael Aubry",
+              support_email: "michael@michaelaubry.com",
+              live_chat_url: "live_chat_url_Value",
+              help_url: "help_url_Value",
+            },
+          });
+
           return db.user.create({
             data: {
               email,
