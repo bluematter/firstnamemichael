3fe94a4cadd1f8d978c7464db067b8993b1730db
bluematter <michael@motionbox.io>
2023-02-16 15:46:23 -0800

    improving adding email UX with loade

--- packages/michaelaubry/src/components/Newsletter.tsx
+++ packages/michaelaubry/src/components/Newsletter.tsx
@@ -21,6 +21,7 @@ interface INewsletterProps {
 export default function Newsletter({ minimal }: INewsletterProps) {
   const [error, setError] = useState<string | null>(null);
   const [success, setSuccess] = useState(null);
+  const [loading, setLoading] = useState<boolean>(false);
 
   const handleSubmit = async (e: SyntheticEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -29,6 +30,8 @@ export default function Newsletter({ minimal }: INewsletterProps) {
     const { email } = Object.fromEntries(formData.entries());
 
     try {
+      setLoading(true);
+
       const data = await request(
         process.env.NODE_ENV === 'development'
           ? 'http://localhost:3001/api'
@@ -44,6 +47,7 @@ export default function Newsletter({ minimal }: INewsletterProps) {
       } else {
         setError(data);
       }
+
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (e: any) {
       if (e.message.includes('Email already exists')) {
@@ -51,6 +55,8 @@ export default function Newsletter({ minimal }: INewsletterProps) {
       } else {
         setError('There was an error');
       }
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -118,7 +124,33 @@ export default function Newsletter({ minimal }: INewsletterProps) {
             className='min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10 sm:text-sm'
           />
           <Button type='submit' className='ml-4 flex-none'>
-            Join
+            {loading ? (
+              <>
+                <svg
+                  className='h-5 w-5 animate-spin text-white'
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                >
+                  <circle
+                    className='opacity-25'
+                    cx='12'
+                    cy='12'
+                    r='10'
+                    stroke='currentColor'
+                    stroke-width='4'
+                  ></circle>
+                  <path
+                    className='opacity-75'
+                    fill='currentColor'
+                    d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
+                  ></path>
+                </svg>
+                Joining...
+              </>
+            ) : (
+              <>Join</>
+            )}
           </Button>
         </div>
         {error ? (
