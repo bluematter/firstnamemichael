95badcb94fe4ca906d332ac06715b43fc9d7dc66
bluematter <michael@motionbox.io>
2023-03-04 11:12:19 -0800

    add meta tags for open graph in _document

--- packages/michaelaubry/src/pages/_document.jsx
+++ packages/michaelaubry/src/pages/_document.jsx
@@ -50,6 +50,19 @@ export default function Document() {
           type='application/feed+json'
           href={`${process.env.NEXT_PUBLIC_SITE_URL}/rss/feed.json`}
         />
+        <meta
+          name='image'
+          content='https://motionbox-public.b-cdn.net/images/og/michaelaubry.png'
+        />
+        <meta
+          property='og:image'
+          content='https://motionbox-public.b-cdn.net/images/og/michaelaubry.png'
+        />
+        <meta
+          name='twitter:image'
+          content='https://motionbox-public.b-cdn.net/images/og/michaelaubry.png'
+        />
+        <meta name='twitter:card' content='summary_large_image'></meta>
       </Head>
       <body className='flex h-full flex-col bg-zinc-50 dark:bg-black'>
         <Main />
